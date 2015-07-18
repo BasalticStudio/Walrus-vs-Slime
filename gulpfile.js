b4ee22e9e7ac66700fbd3f89688f18d1546976cd
蒼時弦也 <elct9620@frost.tw>
2015-07-18 16:07:49 +0800

    Improve js precompile workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,26 @@
 var gulp = require('gulp')
 var sourcemaps = require('gulp-sourcemaps')
 var babel = require('gulp-babel')
+var order = require('gulp-order')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
 var browserSync = require('browser-sync').create()
+
+function handleError(err) {
+    console.log(err.toString())
+    this.emit('end')
+}
 
 gulp.task('js', function() {
     return gulp.src('js/**/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel())
+        .on('error', handleError)
+        .pipe(order([
+            'js/Core.js',
+            'js/**/*.js',
+            'js/game.js'
+        ]))
         .pipe(concat('game.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
